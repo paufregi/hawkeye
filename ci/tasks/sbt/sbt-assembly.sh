d8531713acfc92ef8b7aa419f9840152493f551d
Paul Ellis <paul@movio.co>
2018-09-26 16:35:04 +1200

    Add SRC_PATH param to sbt-assembly task

--- ci/tasks/sbt/sbt-assembly.sh
+++ ci/tasks/sbt/sbt-assembly.sh
@@ -2,10 +2,11 @@
 
 WORKING_FOLDER=$( pwd )
 IVY_FOLDER="$WORKING_FOLDER/.ivy2"
+SOURCE_FOLDER="repo/$SRC_PATH"
 ARTIFACT_FOLDER="$WORKING_FOLDER/artifact"
 VERSION=$(cat version/version)
 
-cd repo
+cd "$SOURCE_FOLDER"
 sbt -ivy "$IVY_FOLDER" clean test assembly
 
 cd target
