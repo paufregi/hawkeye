436eb9f7c2e986a09dc8d303107e240a7871d466
Paul Ellis <paul@movio.co>
2018-10-02 11:42:53 +1300

    Reformat

--- ci/tasks/sbt/assembly.sh
+++ ci/tasks/sbt/assembly.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 WORKING_FOLDER=$( pwd )
-IVY_FOLDER="$WORKING_FOLDER/.ivy2"
-SOURCE_FOLDER="repo/$SRC_PATH"
-ARTIFACT_FOLDER="$WORKING_FOLDER/artifact"
 VERSION=$(cat version/version)
 
-cd "$SOURCE_FOLDER"
-sbt -ivy "$IVY_FOLDER" clean test assembly
+cd "repo/$SRC_PATH"
+sbt -ivy "$WORKING_FOLDER/.ivy2" clean test assembly
 
 cd target
 for f in *.jar; do
-    cp "$f" "$ARTIFACT_FOLDER/${f%.jar}-$VERSION.jar";
+    cp "$f" "$WORKING_FOLDER/artifact/${f%.jar}-$VERSION.jar";
 done
