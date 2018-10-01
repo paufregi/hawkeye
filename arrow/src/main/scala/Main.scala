b72f5487b831d512ba25e6acf25d0486fec06c92
Paul Ellis <paul@movio.co>
2018-10-01 16:10:01 +1300

    Remove timeout setting

--- arrow/src/main/scala/Main.scala
+++ arrow/src/main/scala/Main.scala
@@ -5,14 +5,12 @@ import cats.effect._
 import org.http4s.server.blaze._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
 
 object Main extends App {
   BlazeBuilder[IO]
     .bindHttp(9000, "0.0.0.0")
     .mountService(Endpoints.liveness)
     .mountService(Endpoints.hello)
-    .withIdleTimeout(180 second)
     .serve
     .compile
     .drain
