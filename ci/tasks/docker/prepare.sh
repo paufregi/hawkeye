b3668296a580e993ae01d10b7265ad1df60118fd
Paul Ellis <paul@movio.co>
2018-10-03 09:20:04 +1300

    Remove unnecessary cp

--- ci/tasks/docker/prepare.sh
+++ ci/tasks/docker/prepare.sh
@@ -2,10 +2,6 @@
 
 WORKING_FOLDER=$( pwd )
 
-cp "repo/$DOCKERFILE" docker-src/
+cp repo/$DOCKERFILE_PATH docker-src
 
-if [ -z $RESOURCES ]; then
-    cp "repo/$RESOURCES" docker-src/
-fi
-
-cp "artifact/${NAME}*.jar" docker-src/${NAME}.jar
+cp artifact/${NAME}*.jar docker-src/${NAME}.jar
