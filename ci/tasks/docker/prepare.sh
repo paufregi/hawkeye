6a62c7860c4b562a2ab2fb11aee5c7c807869add
Paul Ellis <paul@movio.co>
2018-10-04 11:29:03 +1300

    Katana concourse pipeline

--- ci/tasks/docker/prepare.sh
+++ ci/tasks/docker/prepare.sh
@@ -6,3 +6,5 @@ cp repo/$DOCKERFILE_PATH docker-src
 
 mkdir docker-src/target
 cp artifact/${NAME}*.jar docker-src/target/${NAME}.jar
+
+echo "Docker will build the image and push on ECR - not doing it cause running Concourse locally in a docker"
