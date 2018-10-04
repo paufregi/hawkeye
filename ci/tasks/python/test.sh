76ebaadbbcdf798a0f96afe95443c86e6d0cc7e6
Paul <paulfrancis.ellis@gmail.com>
2018-10-04 18:33:11 +1300

    CI: Archery - build (#24)
    * Formatting
    
    * Re-enable pytest task
    
    * Python image
    
    * Delete unnecessary Dockerfile
    
    * Remove output from kube apply task
    
    * Show what is in docker-src (debug purpose)
    
    * Add version to archery
    
    * Dockerfile for sub project bow
    
    * Add deployment task
    
    * Add release and deployment steps

--- ci/tasks/python/test.sh
+++ ci/tasks/python/test.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-set -v
-
-echo "installing pipenv"
-
 pip install pipenv
 
-echo "pipenv installed"
-
-#cd "repo/$SRC_PATH"
-#pipenv install -d
-#pipenv run pytest
+cd "repo/$SRC_PATH"
+pipenv install -d
+pipenv run pytest
