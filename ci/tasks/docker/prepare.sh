4b27c6add03976cbebac81fbca6328bfd72d9166
Paul Ellis <paul@movio.co>
2018-10-04 17:35:52 +1300

    Show what is in docker-src (debug purpose)

--- ci/tasks/docker/prepare.sh
+++ ci/tasks/docker/prepare.sh
@@ -7,4 +7,7 @@ cp repo/$DOCKERFILE_PATH docker-src
 mkdir docker-src/target
 cp artifact/${NAME}*.jar docker-src/target/${NAME}.jar
 
+cd $WORKING_FOLDER
+ls -la docker-src/*
+echo ""
 echo "Docker will build the image and push on ECR - not doing it cause running Concourse locally in a docker"
