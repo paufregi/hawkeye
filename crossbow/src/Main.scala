32363716005c75048c8e2be604d659fbd941d723
Paul Ellis <paul@movio.co>
2018-10-09 15:44:20 +1300

    Create crossbow sub-project

---
+++ crossbow/src/Main.scala
@@ -0,0 +1,18 @@
+package lab.acme.crossbow
+
+import language.postfixOps
+import cats.effect._
+import org.http4s.server.blaze._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Main extends App {
+  BlazeBuilder[IO]
+    .bindHttp(9000, "0.0.0.0")
+    .mountService(Endpoints.liveness)
+    .mountService(Endpoints.hello)
+    .serve
+    .compile
+    .drain
+    .unsafeRunSync
+}
