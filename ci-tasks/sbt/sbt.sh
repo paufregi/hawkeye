8f178ba8b1184f921d867f64be86750768b69ac6
Paul Ellis <paul@movio.co>
2018-12-11 12:12:46 +1300

    SBT task

---
+++ ci-tasks/sbt/sbt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export WORKING_FOLDER=$( pwd )
+export COURSIER_CACHE="$WORKING_FOLDER/.coursier"
+
+cat <<EOF > "${WORKING_FOLDER}/repositories"
+[repositories]
+  local
+  maven-central
+  typesafe-ivy-releases: http://repo.typesafe.com/typesafe/ivy-releases/, [organization]/[module]/[revision]/[type]s/[artifact](-[classifier]).[ext], bootOnly
+  sonatype-snapshots: https://oss.sonatype.org/content/repositories/snapshots
+  google-maven-central-mirror: https://maven-central.storage.googleapis.com/repos/central/data/
+EOF
+
+export SBT_OPTS="-Xms512M -Xmx2048M -Xss2M -XX:MaxMetaspaceSize=1024M"
+
+cd "repo/${SRC_PATH}"
+
+
+echo "sbt \"
+echo "-ivy "$WORKING_FOLDER/.ivy2" \"
+echo "-Dsbt.global.base="$WORKING_FOLDER/.sbt/1.0" \"
+echo "-Dsbt.repository.config="$WORKING_FOLDER/repositories" \"
+echo "${COMMAND}"
+
+
+sbt \
+-ivy "$WORKING_FOLDER/.ivy2" \
+-Dsbt.global.base="$WORKING_FOLDER/.sbt/1.0" \
+-Dsbt.repository.config="$WORKING_FOLDER/repositories" \
+${COMMAND}
--- ci/tasks/sbt/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WORKING_FOLDER=$( pwd )
-
-cd "repo/$SRC_PATH"
-sbt -ivy "$WORKING_FOLDER/.ivy2" "project $PROJECT" clean compile test
